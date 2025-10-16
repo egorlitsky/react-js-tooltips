5297b5636dde26f2dcf1265bb619b6cfc3b852d7
egorlitsky <egorlitsky@gmail.com>
2025-10-17 00:23:51 +0300

    Demo app PoC

--- demo-app/src/TooltipDemo.tsx
+++ demo-app/src/TooltipDemo.tsx
@@ -1,27 +1,87 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { TooltipWrapper, Tooltip } from 'react-js-tooltips';
 
 export const TooltipDemo: React.FC = () => {
+  const getTooltipElement = useCallback((tooltipPlacement: string) => {
+    return (
+      <TooltipWrapper
+        renderOverlay={(tooltipProps: any) => {
+          return (
+            <Tooltip {...tooltipProps} width={240}>
+              <div>
+                <h3>Tooltip Title</h3>
+                <p>This is a tooltip with some content.</p>
+              </div>
+            </Tooltip>
+          );
+        }}
+      >
+        <div
+          style={{
+            width: '100px',
+            height: '50px',
+            backgroundColor: 'blue',
+          }}
+        >
+          {tooltipPlacement}
+        </div>
+      </TooltipWrapper>
+    );
+  }, []);
+
   return (
-    <TooltipWrapper
-      renderOverlay={(tooltipProps: any) => {
-        return (
-          <Tooltip {...tooltipProps} width={240}>
-            <div>
-              <h3>Tooltip Title</h3>
-              <p>This is a tooltip with some content.</p>
-            </div>
-          </Tooltip>
-        );
+    <div
+      style={{
+        display: 'flex',
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
       }}
     >
       <div
         style={{
-          width: '100px',
-          height: '50px',
-          backgroundColor: 'blue',
+          width: '100%',
+          height: '100px',
+          backgroundColor: 'red',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
         }}
-      />
-    </TooltipWrapper>
+      >
+        {getTooltipElement('bottom-right')}
+        {getTooltipElement('bottom')}
+        {getTooltipElement('bottom-left')}
+      </div>
+      <div
+        style={{
+          width: '100%',
+          height: '100px',
+          backgroundColor: 'blue',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        {getTooltipElement('right')}
+        {getTooltipElement('top')}
+        {getTooltipElement('left')}
+      </div>
+      <div
+        style={{
+          width: '100%',
+          height: '100px',
+          backgroundColor: 'green',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        {getTooltipElement('top-right')}
+        {getTooltipElement('top')}
+        {getTooltipElement('top-left')}
+      </div>
+    </div>
   );
 };
